ad6918bea270c1c8da2f0e94bd9f9d06579f6154
NithyaArjun <nithyakpa@gmail.com>
2024-04-22 13:21:33 -0400

    second commit

--- src/main/java/com/coderscampus/assignment/Main.java
+++ src/main/java/com/coderscampus/assignment/Main.java
@@ -1,9 +1,12 @@
 package com.coderscampus.assignment;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -16,10 +19,21 @@ public class Main {
 	public static void getData() throws InterruptedException, ExecutionException {
 
 		Assignment8 assignment = new Assignment8();
-		CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(assignment::getNumbers);
-		List<Integer> numbersList = future.get();
-		System.out.println("Fetched numbers: " + numbersList);
-		printOccurrences(numbersList);
+		ExecutorService service = Executors.newFixedThreadPool(50);
+		CompletableFuture<List<Integer>>[] futures = new CompletableFuture[1000];
+		
+		for (int i = 0; i < 1000; i++) {
+			futures[i] = CompletableFuture.supplyAsync(assignment::getNumbers, service);
+		}
+
+		CompletableFuture.allOf(futures).join();
+		List<Integer> allNumbers = new ArrayList<>();
+		
+		for (CompletableFuture<List<Integer>> future : futures) {
+			allNumbers.addAll(future.get());
+		}
+
+		printOccurrences(allNumbers);
 
 	}
 
